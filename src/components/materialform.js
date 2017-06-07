767e657b364d740045498d1f446d5cdf5394d821
juanjuar457 <testjj4573421@gmail.com>
2017-06-07 00:25:54 -0700

    oflv1 fullstack capstone

---
+++ src/components/materialform.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function MaterialForm() {
+    return (
+        <div id="ofl-header">
+            <h1>Order For Later</h1>
+            <form action="submit">
+                <div id="inputlist">
+                    <input type="text" placeholder="placeholder" />
+                    <input type="text" placeholder="placeholder" />
+                    <input type="text" placeholder="placeholder" />
+                    <input type="text" placeholder="placeholder" />
+                </div>
+            </form>
+
+        </div>
+    );
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import MaterialForm from './components/materialform';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <MaterialForm />,
+    document.getElementById('root')
+);
